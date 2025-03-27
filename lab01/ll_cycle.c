4ec9eff1b89712a1f39bb3b43df5509b74865e05
jiangbl <jiangbl@qq.com>
2025-03-27 16:55:02 +0800

    finished lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,18 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    node *tortoise = head;
+    node *hare = head;
+
+    while (hare != NULL) {
+	hare = hare -> next;
+	tortoise = tortoise -> next;
+	if (hare != NULL) {
+	    hare = hare -> next;
+	}
+	if (hare == tortoise) {
+	    return 1;
+	}
+    }
     return 0;
 }
