fd4f3b66580c04978a562b081ac9b1958a4dfbc8
jiangbl <jiangbl@qq.com>
2025-03-28 10:13:37 +0800

    finished lab2 exercise2 lfsr

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,8 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t feedback = ((*reg & 1) ^ ((*reg >> 2) & 1) ^ ((*reg >> 3) & 1) ^ ((*reg >> 5) & 1)) & 1; // Calculate feedback
+    *reg = (*reg >> 1) | (feedback << 15); // Shift right and insert feedback
+    printf("My number is: %u\n", *reg);
 }
 
