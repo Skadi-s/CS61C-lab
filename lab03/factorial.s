1c4cfb2497d2764f25ec40c9f74bcd7865c48e49
jiangbl <jiangbl@qq.com>
2025-04-01 02:19:31 +0800

    finished factorial.s

--- lab03/factorial.s
+++ lab03/factorial.s
@@ -22,3 +22,27 @@ main:
 
 factorial:
     # YOUR CODE HERE
+    # if x equal 0 or 1 return 1
+    beq a0, x0, base_situation
+    addi t0, x0, -1
+    beq t0, x0, base_situation
+    
+    # else return n * factorial (n - 1)
+    addi sp, sp, -8
+    sw ra, 0(sp)
+    sw a0, 4(sp)
+    
+    addi a0, a0, -1
+    jal factorial
+    
+    lw t1, 4(sp)
+    lw ra, 0(sp)
+    addi sp, sp, 8
+    
+    mul a0, a0, t1
+    ret
+    
+base_situation:
+    addi a0, x0, 1
+    ret
+    
